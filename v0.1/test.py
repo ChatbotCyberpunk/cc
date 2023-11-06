250b139b2bd51d2b51f9a688b44209f9f452e306
AndersonHJB <cleland1432803776@icloud.com>
2023-11-06 21:27:13 +0800

    上传CC V0.1 版本源码

---
+++ v0.1/test.py
@@ -0,0 +1,21 @@
+import gradio as gr
+import random
+import time
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(show_share_button=True)
+    msg = gr.Textbox()
+    clear = gr.ClearButton([msg, chatbot])
+
+    def respond(message, chat_history):
+        bot_message = random.choice(["How are you?", "I love you", "I'm very hungry"])
+        chat_history.append((message, bot_message))
+        # print(chat_history)
+        time.sleep(2)
+        return "", chat_history
+
+    msg.submit(respond, [msg, chatbot], [msg, chatbot])
+
+if __name__ == "__main__":
+    demo.launch()
+
